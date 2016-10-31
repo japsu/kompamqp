23f91a6dfadf834c08b1c1c285bef2179b32c34c
Santtu Pajukanta <santtu@pajukanta.fi>
2016-10-31 22:25:28 +0200

    make work

--- kompaq/config.py
+++ kompaq/config.py
@@ -1,6 +1,9 @@
 from envparse import env
 
-AMQP_HOSTNAME = env('AMQP_HOSTNAME', default='localhost')
-AMQP_PORT = env.int('AMQP_PORT', default=5672)
-AMQP_USERNAME = env('AMQP_USERNAME', default='guest')
-AMQP_PASSWORD = env('AMQP_PASSWORD', default='secret')
+env.read_envfile()
+
+AMQP_HOSTNAME = env('KOMPAQ_AMQP_HOSTNAME', default='localhost')
+AMQP_VHOST = env('KOMPAQ_AMQP_VHOST', default='/')
+AMQP_PORT = env.int('KOMPAQ_AMQP_PORT', default=5672)
+AMQP_USERNAME = env('KOMPAQ_AMQP_USERNAME', default='guest')
+AMQP_PASSWORD = env('KOMPAQ_AMQP_PASSWORD', default='guest')
--- kompaq/tool.py
+++ kompaq/tool.py
@@ -1,13 +1,20 @@
 import asyncio
+from pprint import pprint
+
 import asynqp
 
 from . import config
 
 
-async def main():
+def process_message(received_message):
+    pprint(received_message.json())
+
+
+async def setup_tool(process_message=process_message):
     connection = await asynqp.connect(
         config.AMQP_HOSTNAME,
         config.AMQP_PORT,
+        virtual_host=config.AMQP_VHOST,
         username=config.AMQP_USERNAME,
         password=config.AMQP_PASSWORD
     )
@@ -18,13 +25,10 @@ async def main():
     queue = await channel.declare_queue(exclusive=True)
 
     await queue.bind(exchange, '')
-
-    while True:
-        received_message = await queue.get()
-        print(received_message.json())
-        received_message.ack()
+    await queue.consume(process_message)
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(setup_tool())
+    loop.run_forever()
